546906111ea61a2ac4473f605a6052061aae1664
Fush1ren <nightingale.ox@gmail.com>
2025-06-22 19:28:14 +0700

    fix:

--- src/api/v1/auth/auth.controller.ts
+++ src/api/v1/auth/auth.controller.ts
@@ -93,8 +93,8 @@ export const userLogin = async (req: Request, res: Response) => {
 
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'productions',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'lax',
             path: '/',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
